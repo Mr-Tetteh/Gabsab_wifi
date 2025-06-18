97ce7812c07d2a70702abb892bb199eda1821665
Mr. Tetteh <danielstay73@gmail.com>
2025-06-18 20:29:03 +0000

    image

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md content text-accent-foreground">
-    <img src="{{asset('images/homepage/WhatsApp Image 2025-04-08 at 16.25.31.jpeg')}}" alt="">
+    <img src="{{asset('images/ChatGPT Image Jun 16, 2025, 11_17_44 PM.png')}}" alt="image">
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-none font-semibold">GABSAB</span>
--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -9,7 +9,6 @@
 <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
     <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left"/>
 
-
     @if(Auth::user())
         <a href="{{ route('admin.dashboard') }}"
            class="ms-2 me-5 flex items-center space-x-2 rtl:space-x-reverse lg:ms-0"
@@ -283,6 +282,7 @@
                 </div>
             </div>
         </div>
+    </div>
 </flux:footer>
 
 @fluxScripts
